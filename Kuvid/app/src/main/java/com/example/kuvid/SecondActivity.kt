469907e70d29a821e285dcd4c1cd325efd384353
forybh <forybh@naver.com>
2021-06-07 23:12:19 +0900

    공공데이터 API
    연령별/성별 코로나 동향 데이터 받아오기

--- Kuvid/app/src/main/java/com/example/kuvid/SecondActivity.kt
+++ Kuvid/app/src/main/java/com/example/kuvid/SecondActivity.kt
@@ -1,9 +1,8 @@
 package com.example.kuvid
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.PixelCopy.request
-import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,32 +11,28 @@ import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 import java.net.URLDecoder
-import java.net.URLEncoder
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import javax.xml.parsers.DocumentBuilderFactory
-
-
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     var now = LocalDate.now()
     val scope = CoroutineScope(Dispatchers.IO)
     val key : String = "iTpYyrz%2B2quf9rhgNwrICe%2BksA%2B3VK6%2FQ%2FmWVn9UcOUfwTTVzvEnG%2B8MBYTXU2jlsWAOVIuOsrdsROX5t%2Btmrg%3D%3D"
-    var MyDataList = ArrayList<MyData>()
+    var MyDataList = ArrayList<MyData2>()
 
     val date = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 00"))
 
-    lateinit var url : String
+    lateinit var url:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         init()
-
     }
     fun init() {
 
         scope.launch {
             URLDecoder.decode(key, "UTF-8")
-            url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=" +
+            url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19GenAgeCaseInfJson?serviceKey=" +
                     key + "&pageNo=1&numOfRows=10&STD_DAY=${date}}"
             val xml: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url)
             xml.documentElement.normalize()
@@ -56,13 +51,13 @@ class MainActivity : AppCompatActivity() {
                         map.putIfAbsent(elem.attributes.item(j).nodeName, elem.attributes.item(j).nodeValue)
                     }
                     val gubun = elem.getElementsByTagName("gubun").item(0).textContent
-                    val def_cnt: Int = elem.getElementsByTagName("defCnt").item(0).textContent.toInt()
-                    val iso_ing_cnt : Int = elem.getElementsByTagName("isolIngCnt").item(0).textContent.toInt()
-                    val death_cnt : Int = elem.getElementsByTagName("deathCnt").item(0).textContent.toInt()
-                    val iso_clear_cnt = elem.getElementsByTagName("isolClearCnt").item(0).textContent.toInt()
-                    val inc_dec : Int = elem.getElementsByTagName("incDec").item(0).textContent.toInt()
+                    val conf_case: Int = elem.getElementsByTagName("confCase").item(0).textContent.toInt()
+                    val conf_case_rate  = elem.getElementsByTagName("confCaseRate").item(0).textContent.toFloat()
+                    val death : Int = elem.getElementsByTagName("death").item(0).textContent.toInt()
+                    val death_rate = elem.getElementsByTagName("deathRate").item(0).textContent.toFloat()
+                    val critical_rate  = elem.getElementsByTagName("criticalRate").item(0).textContent.toFloat()
 
-                    val data:MyData = MyData(gubun, def_cnt, iso_ing_cnt, death_cnt, iso_clear_cnt, inc_dec)
+                    val data:MyData2 = MyData2(gubun, conf_case, conf_case_rate, death, death_rate, critical_rate)
                     MyDataList.add(data)
                     Log.d("data", data.toString())
 
@@ -71,7 +66,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-
-
-
